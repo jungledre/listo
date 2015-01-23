2356fb87b85e3388910d9d8e12db3179b0214643
Alyssa <alyssaw90@gmail.com>
2015-01-23 15:54:52 -0800

    models and assocations

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -1,4 +1,4 @@
 class Flag < ActiveRecord::Base
-  belongs_to :reports, class_name: "User", foreign_key: "reporter_id"
-  belongs_to :flags, class_name: "User", foreign_key: "flagged_id"
+  belongs_to :reporter, class_name: "User", foreign_key: "reporter_id"
+  belongs_to :flagged, class_name: "User", foreign_key: "flagged_id"
 end
