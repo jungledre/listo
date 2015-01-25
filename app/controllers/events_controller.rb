b261fbedc602a7c731b2bc85e7c5fe62f246b531
Adrienne <adrie.k@gmail.com>
2015-01-25 02:35:01 -0800

    Added search parameters

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,11 +11,12 @@ class EventsController < ApplicationController
     @page = 'event'
 
     response = JSON.parse(
-      open('https://api.foursquare.com/v2/venues/explore?near=ballard+wa&client_id='+
-      ENV["FOURSQUARE_KEY"]+'&client_secret='+ENV["FOURSQUARE_SECRET"]+'&v=20140806').read)
-    @venue = response['response']['groups'].first['items'].first['venue']
-    @venue_name = @venue["name"]
-    @venue_location = @venue["location"]["address"]
+      open('https://api.foursquare.com/v2/venues/explore?near=ballard+wa&price=1,2&client_id='+
+      ENV['FOURSQUARE_KEY']+'&client_secret='+ENV['FOURSQUARE_SECRET']+'&v=20140806').read)
+    @venues = response['response']['groups'].first['items']
+    @venue = @venues[rand(1..@venues.length)]['venue']
+    @venue_name = @venue['name']
+    @venue_location = @venue['location']['address']
  end
 
   def flake
