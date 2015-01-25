c2f8dcf1276ba1aba4632cab304bbd696543c1d4
Adrienne <adrie.k@gmail.com>
2015-01-25 02:16:19 -0800

    Foursquare API test is now on the events show page.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,22 @@
 class EventsController < ApplicationController
 
+  require 'open-uri'
+  require 'json'
+
   def create
 
   end
 
   def show
-    @page = "event"
+    @page = 'event'
 
-  end
+    response = JSON.parse(
+      open('https://api.foursquare.com/v2/venues/explore?near=ballard+wa&client_id='+
+      ENV["FOURSQUARE_KEY"]+'&client_secret='+ENV["FOURSQUARE_SECRET"]+'&v=20140806').read)
+    @venue = response['response']['groups'].first['items'].first['venue']
+    @venue_name = @venue["name"]
+    @venue_location = @venue["location"]["address"]
+ end
 
   def flake
 
