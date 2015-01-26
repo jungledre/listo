043d99d9b7889714b0b2e1642164760e208ada93
Alyssa <alyssaw90@gmail.com>
2015-01-26 12:17:58 -0800

    random activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,6 +5,17 @@ class ActivitiesController < ApplicationController
   def index
     @page = "activities"
     @user = current_user
+    activities = Activity.all
+    @activity1 = activities[rand(1...activities.length)].name
+
+    while @activity2 == @activity1
+      @activity2 = activities[rand(1...activities.length)].name
+    end
+
+    while @activity3 == @activity1 || @activity3 == @activity2
+      @activity3 = activities[rand(1...activities.length)].name
+    end
+    
   end
 
 end
